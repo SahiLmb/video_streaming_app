38dc6424ec63f3db42c46095995b2d14ac1367b9
Sahil <sahilbodke10@gmail.com>
2024-05-26 19:48:13 +0530

    meeting room page

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+// Video Streaming setup
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+'use client'
+
+import Loader from '@/components/Loader';
+import VmeetRoom from '@/components/VmeetRoom';
+import VmeetSetup from '@/components/VmeetSetup';
+import { useGetCallById } from '@/hooks/useGetCallByID';
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
+import React, { useState } from 'react'
+
+const Meeting = ({ params: { id } }: { params: { id: string } }) => {
+
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false)
+
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if(!isLoaded || isCallLoading) return <Loader />
+
   return (
-    <div>Meeting Room: #{params.id}</div>
+    <main className='h-screen w-full'>
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <VmeetSetup setIsSetupComplete = {setIsSetupComplete}/>
+          ): (  
+            <VmeetRoom />
+          )}
+          </StreamTheme>
+      </StreamCall>
+
+    </main>
   )
 }
 
