07439a423dc6dd990d633e61a3697c7ec44bad6d
Sahil <sahilbodke10@gmail.com>
2024-05-26 19:42:55 +0530

    videostream layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,26 @@
 
+
+import StreamVideoApi from '@/provider/StreamClientProvider'
+import { Metadata } from 'next';
 import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "Vmeet",
+  description: "A real time video facing app",
+  icons: {
+    icon: '/icons/logo1.svg'
+  }
+};
+
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
 
     <main>
-        {children}
+      <StreamVideoApi>
+      {children}
+      </StreamVideoApi>
+  
         </main>
   )
 }
